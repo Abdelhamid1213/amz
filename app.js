c9544d5a8b2d0cb166af8d95d8dee65d66f6a33e
samaanl <nasifasayed@gmail.com>
2023-09-28 22:53:08 +0530

    simplified the js code and also added class atrribute to li instead of id

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-document.getElementById("add-btn").addEventListener("click", addTask);
-
 function addTask() {
-	var task = document.getElementById("task-input").value;
-	document.getElementById("task-input").value = '';
-	console.log(task);
-	if (task) {
-		var ul = document.getElementById("list");
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(task));
-		li.setAttribute("id", "task");
-		ul.appendChild(li);
-	}
-	
+  var task = document.getElementById("task-input").value;
+  document.getElementById("task-input").value = "";
+  console.log(task);
+  if (task) {
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.innerText = task;
+    li.setAttribute("class", "task");
+    ul.appendChild(li);
+  }
 }
