85285d801eaa4139f138469eb9925ffaf1a95ca8
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2023-10-03 12:12:44 +0100

    Added basic js functionality

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+let list = document.getElementById('list');
+
 function addTask() {
   var task = document.getElementById("task-input").value;
   document.getElementById("task-input").value = "";
-  console.log(task);
   if (task) {
     var ul = document.getElementById("list");
     var li = document.createElement("li");
+	var close = document.createElement("button");
     li.innerText = task;
     li.setAttribute("class", "task");
     ul.appendChild(li);
+	close.innerText = "X"
+	close.setAttribute("class", "close");
+	li.appendChild(close);
   }
 }
+
+list.addEventListener('click', function(element){
+	if (element.target.tagName === "LI"){
+		element.target.classList.toggle("done");
+	} else if (element.target.tagName === "BUTTON"){
+		element.target.parentElement.remove();
+	}
+});
