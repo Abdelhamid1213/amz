bbd5d3dd8b1b22233b85607cd23c15db5d578322
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2023-11-01 13:29:49 +0100

    Major Updates

--- app.js
+++ app.js
@@ -1,37 +1,63 @@
-let list = document.getElementById('list');
+const list = document.getElementById('list');
+const taskInput = document.getElementById('task-input');
 
 function addTask() {
-  var task = document.getElementById("task-input").value;
-  document.getElementById("task-input").value = "";
+  const task = taskInput.value.trim();
+  taskInput.value = "";
   if (task) {
-    var ul = document.getElementById("list");
-    var li = document.createElement("li");
-	var close = document.createElement("button");
+    const li = document.createElement("li");
     li.innerText = task;
-    li.setAttribute("class", "task");
-    ul.appendChild(li);
-	close.innerText = "\u00d7"
-	close.setAttribute("class", "close");
-	li.appendChild(close);
-    saveTasks()
+    li.classList.add("task");
+    
+    const close = document.createElement("button");
+    close.innerText = "\u00d7";
+    close.classList.add("close");
+    
+    li.appendChild(close);
+    list.appendChild(li);
+    
+    saveTasks();
+    updateClearAllButton();
   }
 }
 
-list.addEventListener('click', function(element){
-	if (element.target.tagName === "LI"){
-		element.target.classList.toggle("done");
-        saveTasks()
-	} else if (element.target.tagName === "BUTTON"){
-		element.target.parentElement.remove();
-        saveTasks()
-	}
+function handleKeyPress(event) {
+  if (event.key === 'Enter') {
+    addTask();
+  }
+}
+
+list.addEventListener('click', function (event) {
+  const target = event.target;
+  if (target.tagName === "LI") {
+    target.classList.toggle("done");
+    saveTasks();
+  } else if (target.tagName === "BUTTON") {
+    target.parentElement.remove();
+    saveTasks();
+  }
+
+  updateClearAllButton();
 });
 
+function clearAllTasks() {
+  list.innerHTML = "";
+  saveTasks();
+  updateClearAllButton();
+}
+
 function saveTasks() {
-    localStorage.setItem('tasks', list.innerHTML);
+  localStorage.setItem('tasks', list.innerHTML);
 }
 
 function fetchTasks() {
-    list.innerHTML = localStorage.getItem('tasks');
+  list.innerHTML = localStorage.getItem('tasks') || "";
+  updateClearAllButton();
 }
-fetchTasks()
+
+function updateClearAllButton() {
+  const clearAllBtn = document.getElementById('clear-all-btn');
+  clearAllBtn.style.display = list.children.length > 0 ? 'block' : 'none';
+}
+
+fetchTasks();
